a8b29b690dc8e463e329e35466968aeb3d6025f1
Aneudys <Aneudys@192.168.1.21>
2018-02-07 22:58:02 -0500

    first real push

---
+++ Lab 3.2/src/Sortingpart2.java
@@ -0,0 +1,39 @@
+
+public class Sortingpart2 
+{
+		public static String[] merge(String [] arr1, String[] arr2)
+		{
+				String[] result = new String[arr1.length + arr2.length];
+				int dex1 = 0;
+				int dex2 = 0;
+				int dexresult = 0;
+				while(dexresult < result.length)
+				{
+					if (dex1 == arr1.length)
+						{
+							result[dexresult] = arr2[dex2];
+							dex2++;
+						}
+					
+					else if(dex2 == arr2.length)
+						{
+							result[dexresult] = arr1[dex1];
+							dex1++;
+						}
+					
+					else if (arr1[dex1].compareTo(arr2[dex2]) <= 0)
+						{
+							result[dexresult] = arr1[dex1];
+							dex1++;
+						}
+					
+					else if (arr1[dex1].compareTo(arr2[dex2]) > 0)
+						{
+							result[dexresult] = arr2[dex2];
+							dex2++;
+						}
+					dexresult++;
+				}
+				return result;
+		  }
+	}
